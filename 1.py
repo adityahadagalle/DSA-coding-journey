314cee44ced8248a5c74fac81808de99e03310aa
charusheel <rv.charusheel@gmail.com>
2026-02-16 22:51:29 +0530

    Solved interval overlap problem

--- 1.py
+++ 1.py
@@ -1188,20 +1188,32 @@ from collections import Counter
 
 
 
-intervals = [[1,2], [2,3], [3,4],[1,3]]
+# intervals = [[1,2], [2,3], [3,4],[1,3]]
+# intervals.sort(key=lambda x: x[1])
+
+# end1=intervals[0][1]
+# count=0
+# for i in range(1,len(intervals)):
+#     st2=intervals[i][0]
+#     end2=intervals[i][1]
+#     if end1>st2:
+#         count+=1
+#     else:
+#         end1=end2 
+# print(count)      
 
 
 
+intervals = [[1,10],[2,3],[4,5],[6,7]]
 
-intervals.sort(key=lambda x: x[1])
-
+intervals.sort(key=lambda x:x[1])
+count=1
 end1=intervals[0][1]
-count=0
 for i in range(1,len(intervals)):
     st2=intervals[i][0]
     end2=intervals[i][1]
-    if end1>st2:
+    if end1<=st2:
         count+=1
-    else:
-        end1=end2 
-print(count)      
+    
+        end1=end2
+print(count)        
--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,1 +1,13 @@
-    # end1=end2  
+intervals = [[1,2], [2,3], [3,4],[1,3]]
+# intervals.sort(key=lambda x: x[1])
+
+# end1=intervals[0][1]
+# count=0
+# for i in range(1,len(intervals)):
+#     st2=intervals[i][0]
+#     end2=intervals[i][1]
+#     if end1>st2:
+#         count+=1
+#     else:
+#         end1=end2 
+# print(count)   
