db57fe52cdbe9f64429919beb4045b70afae9594
adityahadagalle <adityahadgalle@gmail.com>
2026-03-01 11:23:58 +0530

    commite

--- 1.py
+++ 1.py
@@ -1367,14 +1367,13 @@ from collections import Counter
 # while [i%n]!=i:
 #     print(i)
 #     i+=1
-nums = [8,4,3,5,7]
-
+nums = [1,2,3,4,5]
 n = len(nums)
 result = [-1] * n
 stack = []
 
 for i in range(2 * n):
-    while stack and nums[stack[-1]] < nums[i % n]:
+    while stack and nums[stack[-1]] <nums[i % n]:
         index = stack.pop()
         result[index] = nums[i % n]
     
@@ -1382,3 +1381,4 @@ for i in range(2 * n):
         stack.append(i)
 
 print(result)
+
