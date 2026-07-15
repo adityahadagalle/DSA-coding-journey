f0b9136b79b690b55bfb7906edd50e67f50bebae
adityahadagalle <adityahadgalle@gmail.com>
2026-07-15 23:30:59 +0530

    Initial commit: Tomato Disease Detection project

--- practise1.py/2.py
+++ practise1.py/2.py
@@ -84,9 +84,160 @@
         
         
     
+    
+    
+    
+######################recursion#################################################recursion#################################################recursion#################################################recursion###########################
+
+# def fun(count):
+#     if count==3:
+#         return
+#     print("Adii")
+#     count+=1
+#     fun(count)
+# fun(count=0)  
 
 
-nums = [2,1,2]
-k = 1
-for i in range(len(nums)):
-    pre
+
+# def fun(i,n):
+#     if i==6:
+#         return
+#     print(i)
+#     fun(i+1,n)
+# fun(i=1,n=6)  
+
+
+# def fun(i,n):
+#     if i<n:
+#         return
+#     print(i)
+#     fun(i-1,n)
+# fun(10,1)      
+    
+    
+# def fun(n):
+#     if n==1:
+#         return 1
+#     return n*fun(n-1)    
+
+# print((fun(5)))
+
+
+# def fun(n):
+#     if n==1:
+#         return 1
+#     return n+fun(n-1)
+# print(fun(10))
+    
+# i=0
+# j=0  
+# def fun(i,j,arr):
+
+#     if i>=j:
+#         return True
+#     if arr[i]==arr[j]:
+#         i+=1
+#         j-=1
+#     else:
+#         return False
+#     return fun(i,j,arr)
+# print(fun(i=0,j=4,arr="madam") )   
+
+
+# def fun(x,n):
+#     if n==1:
+#         return x
+#     return x*fun(x,n-1)
+# print(fun(2,5))
+
+
+
+
+# def fun(n):
+#     if n==0:
+#         return 1
+#     if n==1:
+#         return 1
+#     return fun(n-1)+fun(n-2)
+# print(fun(5))
+
+# def fun(n,m):
+#     if n==1 or m==1:
+#         return 1
+#     return fun(n,m-1)+fun(n-1,m)
+# print(fun(3,3))
+
+
+# def fun(i,arr):
+#     if i >= len(arr):
+#         return 0
+#     rob=arr[i]+fun(i+2,arr)
+#     skip=fun(i+1,arr)
+#     return max(rob,skip)
+# print(fun(0,[2,7,9,3,1]))
+
+
+
+# def fun(i,arr):
+#     if i>=len(arr):
+#         return 0
+#     rob=arr[i]+fun(i+2,arr)
+# #     skip=arr[i+1]
+# #     return max(rob,skip)
+# # print(fun(i=0,arr=[3,2,7,10]))
+
+# ###########################backtracking#####################################################################
+
+# def backtrack(index):
+
+#     if index == len(nums):
+#         ans.append(path[:])
+#         return
+
+#     # Take
+#     path.append(nums[index])
+#     backtrack(index+1)
+
+#     # Undo
+#     path.pop()
+
+#     # Skip
+#     backtrack(index+1)
+
+
+# nums = [1,2,3]
+
+# ans = []
+# path = []
+
+# backtrack(0)
+
+# print(ans)         
+
+
+def backtrack(index):
+
+    if index == len(nums):
+        ans.append(path[:])
+        return
+
+    # Take
+    if path[-1]!=path[:]:
+        path.append(nums[index])
+    backtrack(index+1)
+
+    # Undo
+    path.pop()
+
+    # Skip
+    backtrack(index+1)
+
+
+nums = [1,2,2]
+
+ans = []
+path = []
+
+backtrack(0)
+
+print(ans)  
