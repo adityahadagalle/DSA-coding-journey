d4a149044af3a718ff1327569c358b3f4c048a05
Aditya Hadagalle <adityahadgalle@gmail.com>
2026-08-02 00:39:07 +0530

    committed

--- practise1.py/2.py
+++ practise1.py/2.py
@@ -723,3 +723,62 @@
 # backtrack(0,[1,1,2,2,3,3],3,0)
 # print(ans      
             
+            
+#trees########################################################   ############################   ############################   ############################   ############################   ############################   ############################   ############################   ############################               
+
+
+from collections import deque
+class Node:
+    def __init__(self, val):
+        self.val = val
+        self.left = None
+        self.right = None
+
+
+root = Node(1)
+
+root.left = Node(2)
+root.right = Node(3)
+
+root.left.left = Node(4)
+root.left.right = Node(5)
+
+root.right.left = Node(6)
+root.right.right = Node(7)
+
+root.left.left.left = Node(8)
+root.left.left.right = Node(9)
+
+root.right.left.right = Node(10)
+
+queue = deque([root])
+ans = []
+used=True
+while queue:
+
+    level = []
+
+    size = len(queue)
+
+    for _ in range(size):
+
+        node = queue.popleft()
+
+        level.append(node.val)
+            
+        if node.left:
+                queue.append(node.left)
+
+        if node.right:
+                queue.append(node.right)
+
+                
+    if used:    
+        ans.append(level)
+        used=False
+    else:
+        level.reverse()
+        ans.append(level)
+        used=True   
+
+print(ans)
